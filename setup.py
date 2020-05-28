cd400ead5afba0ec17597d09b06b50b97c562e0b
Zach Wilson <zachwilson130@gmail.com>
2020-05-28 13:34:17 -0700

    adding PIP info for Robinhood trader

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="robinhood-scraper-nlp", # Replace with your own username
+    version="0.0.1",
+    author="Zachary Wilson",
+    author_email="zachwilson130@gmail.com",
+    description="A package that interacts with Robinhood APIs to access news about stocks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EcZachly/robinhood-nlp",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
